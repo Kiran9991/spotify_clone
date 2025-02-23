464ff51e6e721c2298c91aebef0f6d4a828d393c
Kiran9991 <kiranagiwale@gmail.com>
2025-02-23 13:14:43 +0530

    made connection of node js to mongodb

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 const port = 8000;
+
+console.log(process.env.MONGO_USERNAME,process.env.MONGO_PASSWORD)
+
+// connect mongodb to our node app.
+// mongooose.connect() takes 2 arguments: 1. which db to connect to (db, url), 2. 2. connection options
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.t9elw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+).then((data) => console.log('connected to mongo!'))
+.catch((error) => console.log('failed to connect to mongo!',error))
+
+
 
 // API : GET type : / : return text "hello world"
 app.get('/', (req, res) => {
